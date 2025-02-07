70b9e0990323fa46c0c0ba1bf3d0683a30ced698
MartaDiazPerez <marta.diazp@edu.uah.es>
2025-02-07 20:03:31 +0100

    Ejercicio circle

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/e1/Circle.java
@@ -0,0 +1,31 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.e1;
+
+/**
+ * Atribute circle
+ */
+public class Circle {
+    private double  radius;
+    public Circle() {
+        radius = 1.0;
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle[radius=" + radius +"]";// Aquí queremos que nos lo retorne de esra forma
+    }
+}

b365f37074cbbea5e08a0b291e041864f717c527
MartaDiazPerez <marta.diazp@edu.uah.es>
2025-02-09 18:42:49 +0100

    Ejercicio Employee

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/e3/Employee.java
@@ -0,0 +1,45 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.e3;
+// ZONA DE ATRIBUTOS
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    // ZONA DE METODOS
+    public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public int getAnnualSalary() {
+        return salary * 12;
+    }
+    public int raiseSalary(int percentage) {
+        int newSalary = salary + percentage*salary/100;
+        this.salary = newSalary;
+        return newSalary;
+    }
+    @Override
+    public String toString() {
+        return "Employee[id=" + id + ",name=" + firstName + " " + lastName + ",salary=" + salary +"]";
+    }
+}
